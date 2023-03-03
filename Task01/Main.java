d1eaea88a91ec40614f13bc9db01705c40a582ea
MargoBB <115227124+MargoBB@users.noreply.github.com>
2023-03-03 19:24:04 +0200

    Update Main.java

--- Task01/Main.java
+++ Task01/Main.java
@@ -2,14 +2,13 @@
 * Print command-line parameters.
 */
 public class Main {
-    /**
-    * Program entry point.
-    * @param args command-line parameters list
-    */
-
+/**
+* Program entry point.
+* @param args command-line parameters list
+*/
 public static void main(String[] args) {
-for (String s: args) {
-System.out.println(s);
+for (String arg: args) {
+System.out.println(arg);
 }
 }
 }
