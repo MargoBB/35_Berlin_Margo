c85773006126c659514c4a8e77f23a06c25962c4
MargoBB <115227124+MargoBB@users.noreply.github.com>
2023-03-12 16:41:17 +0200

    v1

--- Task01/BinaryUnitCounter.java
+++ Task01/BinaryUnitCounter.java
@@ -1,18 +1,15 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class BinaryUnitCounter implements Serializable {
     private static final long serialVersionUID = 1L;
     private double[] args;
     private double[] results;
     private transient int binaryUnitCount; // transient поле
-    private int lastBinaryUnitCount;
     
     public BinaryUnitCounter(double[] args) {
         this.args = args;
         this.results = new double[args.length];
         this.binaryUnitCount = 0;
-        this.lastBinaryUnitCount = 0;
     }
     
     public void calculate() {
@@ -25,18 +22,9 @@ public class BinaryUnitCounter implements Serializable {
         String binaryStr = Integer.toBinaryString((int)mean);
         binaryUnitCount = binaryStr.length();
     }
-
-    public void calculateAndUndo() {
-        calculate();
-        undo();
-    }
     
     public int getBinaryUnitCount() {
         return binaryUnitCount;
-    }
-
-    public void undo() {
-        binaryUnitCount = lastBinaryUnitCount;
     }
     
     public String toString() {
@@ -56,35 +44,6 @@ public class BinaryUnitCounter implements Serializable {
     public static void main(String[] args) throws Exception {
         double[] arguments = {0.1, 0.2, 0.3, 0.4};
 
-        BinaryUnitCounter counter = new BinaryUnitCounter(arguments);
-        Scanner scanner = new Scanner(System.in);
-
-        boolean done = false;
-        while (!done) {
-            System.out.print("Enter command (c=calculate, u=undo, q=quit): ");
-            String command = scanner.nextLine();
-            
-            switch (command) {
-                case "c":
-                    counter.calculate();
-                    System.out.println("Binary unit count: " + counter.getBinaryUnitCount());
-                    counter.lastBinaryUnitCount = counter.getBinaryUnitCount();
-                    break;
-                case "u":
-                    counter.undo();
-                    System.out.println("Undone. Binary unit count: " + counter.getBinaryUnitCount());
-                    break;
-                case "q":
-                    done = true;
-                    break;
-                default:
-                    System.out.println("Invalid command. Try again.");
-                    break;
-            }
-        }
-    
-        counter.calculateAndUndo();
-        
         BinaryUnitCounter buc = new BinaryUnitCounter(arguments);
         buc.calculate();
         System.out.println(buc);
